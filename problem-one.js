43c9b159c6b8c31905d29d0cec48bcc2d6144b3e
aihridoy <cse_1912020168@lus.ac.bd>
2025-07-14 15:26:58 +0600

    sol: problem-one

---
+++ problem-one.js
@@ -0,0 +1,18 @@
+const people = [
+  { name: "John", age: 25, gender: "male" },
+  { name: "Sarah", age: 30, gender: "female" },
+  { name: "Mike", age: 35, gender: "male" },
+  { name: "Emily", age: 28, gender: "female" },
+  { name: "David", age: 32, gender: "male" },
+  { name: "Lisa", age: 27, gender: "female" },
+  { name: "Tom", age: 29, gender: "male" }
+];
+
+function getMaleNames(peopleArray) {
+  return peopleArray
+    .filter(person => person.gender === "male") 
+    .map(person => person.name);      
+}
+
+const maleNames = getMaleNames(people);
+console.log("Male names:", maleNames);
